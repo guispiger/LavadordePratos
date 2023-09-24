5de5e2d9aa96a6b0bda1656fc8459a0c2ee166c3
guispiger <guispiger@hotmail.com>
2023-09-24 09:44:22 -0300

    atualizados comentarios

--- src/src/Escorredor.java
+++ src/src/Escorredor.java
@@ -1,14 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package src;
 
 import java.util.LinkedList;
 
 /**
  *
- * @author aluno
+ * @author guispiger
  */
 public class Escorredor {
     private final int max = 10;
